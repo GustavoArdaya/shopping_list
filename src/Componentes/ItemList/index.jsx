c2a4fb1b1be0843c48fe3e6a9a4dd491761de0fd
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-01-24 12:11:24 +0100

    create delete function

--- src/Componentes/ItemList/index.jsx
+++ src/Componentes/ItemList/index.jsx
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-let productList = [{
-  id: "1",
-  name: "Patatas",
-  isBought: false
-},
-{
-  id: "2",
-  name: "Leche",
-  isBought: true
-},
-{
-  id: "3",
-  name: "Huevos",
-  isBought: false
-},
-]
+let productList = [
+  {
+    id: '1',
+    name: 'Patatas',
+    isBought: false,
+  },
+  {
+    id: '2',
+    name: 'Leche',
+    isBought: true,
+  },
+  {
+    id: '3',
+    name: 'Huevos',
+    isBought: false,
+  },
+];
 
 export default function ItemList() {
-   
-  
+  const [products, setProducts] = useState(productList);
+
+  const deleteById = (idItem) => {
+    // console.log('funcionoooo!');
+    let newData = products.filter((item) => item.id !== idItem);
+
+    console.log(newData);
+    setProducts(newData);
+  };
+
   return (
-  <div>
-    <h2>Lista de Compra</h2>
-    <ul> {productList.map(item=> 
-      <li key={item.id}>{item.name}</li>
-      )}
-
-    </ul>
-    
-
-  </div>);
+    <div>
+      <h2>Lista de Compra</h2>
+      <ul>
+        {productList.map((item) => (
+          <li key={item.id}>
+            {item.name}
+            <button onClick={() => deleteById(item.id)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-
-
