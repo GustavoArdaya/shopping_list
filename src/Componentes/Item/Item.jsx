5a8f1a52fafbe1cc03e67cb338e3326c1742b62b
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-01-26 17:58:25 +0100

    add styles items

--- src/Componentes/Item/Item.jsx
+++ src/Componentes/Item/Item.jsx
@@ -1,23 +1,28 @@
-import React from "react";
-import styles from "./Item.module.css"
+import React from 'react';
+import styles from './Item.module.css';
 import { GrEdit } from 'react-icons/gr';
-import { RiDeleteBin5Line } from "react-icons/ri";
-
+import { RiDeleteBin5Line } from 'react-icons/ri';
 
 export default function Item({ item, deleteById, editById }) {
   return (
-    <div className= {styles.liContainer}>
-      <li  className= {styles.item}>
-        <div>
-          {item.name}
-        </div>
+    <div className={styles.liContainer}>
+      <li
+        style={
+          item.isBought
+            ? { backgroundColor: 'green' }
+            : { backgroundColor: 'red' }
+        }
+        className={styles.item}
+      >
+        <div>{item.name}</div>
         <div className={styles.iconsContainer}>
-            <GrEdit onClick={() => editById(item.id)}/>
-            <RiDeleteBin5Line onClick={() => deleteById(item.id)}/>
-            {/* <button onClick={() => editById(item.id)}>Editar</button> */}
-            {/* <button onClick={() => deleteById(item.id)}>Eliminar</button> */}
+          <GrEdit onClick={() => editById(item.id)} />
+          <RiDeleteBin5Line onClick={() => deleteById(item.id)} />
+          {/* <button onClick={() => editById(item.id)}>Editar</button> */}
+          {/* <button onClick={() => deleteById(item.id)}>Eliminar</button> */}
         </div>
       </li>
     </div>
   );
 }
+// className={item.isBought ? 'verde' : 'rojo'}
