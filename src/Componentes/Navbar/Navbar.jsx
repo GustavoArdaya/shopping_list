e1cc8200b5737d7b339ee90b35e7187bbc28b439
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-01-27 09:33:22 +0100

    add styles

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from './navbar.module.css';
 
 export default function Navbar() {
   return (
-    <div>
-        <img></img>
-        <h1>Shopping List</h1>
+    <div className={styles.navContainer}>
+      {/* <img source='./img/logo.png' alt='logo' /> */}
+      <h1 className={styles.textNavbar}>Shopping List</h1>
     </div>
-    
-  )
+  );
 }
