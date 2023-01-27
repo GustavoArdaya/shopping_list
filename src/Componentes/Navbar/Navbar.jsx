d360a92329de0d713121bff9c5ce31a4f9ca048d
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-01-27 09:47:05 +0100

    formated navbar

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -4,8 +4,11 @@ import styles from './navbar.module.css';
 export default function Navbar() {
   return (
     <div className={styles.navContainer}>
-      {/* <img source='./img/logo.png' alt='logo' /> */}
-      <h1 className={styles.textNavbar}>Shopping List</h1>
+      <img className={styles.navImage} src={require('./img/logo.png')} />
+      <div className={styles.textNavbar}>
+      <h1>Shopping </h1>
+      <h1>List</h1>
+      </div>
     </div>
   );
 }
